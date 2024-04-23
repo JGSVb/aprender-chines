b5be0e304adf0cc9b43a68aeb28fa6a20c68c5ce
Alexandre Vieira <59124659+JGSVb@users.noreply.github.com>
2024-04-23 17:04:46 +0100

    Add files via upload

--- static/overlay.js
+++ static/overlay.js
@@ -1,29 +1,12 @@
-const PURPOSE_CREATECARD = "createAnkiCardDialog";
-const PURPOSE_SHOWCARDS = "ankiCardsDialog";
-
 const Overlay = new class {
-
 	#element = document.getElementById("overlay");
-
-	#purpose_create_elem = document.getElementById(PURPOSE_CREATECARD);
-	#purpose_show_elem = document.getElementById(PURPOSE_SHOWCARDS);
-	#purpose_create_display = this.#purpose_create_elem.style.display;
-	#purpose_show_display = this.#purpose_show_elem.style.display;
+	#content = document.getElementById("overlayContent");
 
 	#visible = false;
-	#purpose = ""
 
-	setPurpose(p){
-		this.#purpose_create_elem.style.display = "none";
-		this.#purpose_show_elem.style.display = "none";
-
-		this.#purpose = p;
-
-		if(p == this.#purpose_create_elem.id){
-			this.#purpose_create_elem.style.display = this.#purpose_create_display;
-		} else {
-			this.#purpose_show_elem.style.display = this.#purpose_show_display;
-		}
+	setContent(c){
+		this.#content.innerHTML = "";
+		this.#content.appendChild(c);
 	}
 
 	setVisible(v) {
--- static/showAnkiCardsButton.js
+++ static/showAnkiCardsButton.js
@@ -1,26 +1,56 @@
 const ShowAnkiCardsDialog = {
-	ankiCardsDialog: document.getElementById("ankiCardsDialog"),
+	dialogTemplate: document.getElementById("ankiCardsDialogTemplate"),
 	ankiCardEntryTemplate: document.getElementById("ankiCardEntryTemplate"),
-	ankiCardsContainer: document.getElementById("ankiCardsContainer"),
+	deleteAnkiCardConfirmationTemplate: document.getElementById("deleteAnkiCardConfirmationTemplate"),
+	dialog: null,
+	ankiCardsDialog: null,
+	ankiCardsContainer: null,
+
+	showDeleteCardConfirmation: function(entry, index){
+		const deleteConfirmation = document.importNode(this.deleteAnkiCardConfirmationTemplate.content, true);
+
+		deleteConfirmation.getElementById("return").onclick = function(){
+			this.show();
+		}.bind(this);
+
+		Overlay.setContent(deleteConfirmation);
+	},
 
 	populate: function(json){
 		this.ankiCardsContainer.innerHTML = "";
 
-		json.forEach(entry => {
+		json.reverse();
+
+		for(let i = 0; i < json.length; i++){
+			const entry = json[i];
+
 			const clone = document.importNode(this.ankiCardEntryTemplate.content, true);
 			clone.getElementById("hanzi").innerHTML = entry[0];
 			clone.getElementById("pinyin").innerHTML = entry[1];
 			clone.getElementById("example").innerHTML = entry[2];
 			clone.getElementById("meaning").innerHTML = entry[3];
-			this.ankiCardsContainer.appendChild(clone);
 
-		})
+			clone.getElementById("deleteAnkiCard").onclick = function(){
+				this.showDeleteCardConfirmation(entry, i);
+			}.bind(this);
+
+			clone.getElementById("modifyAnkiCard").onclick = function(){
+				createAnkiCardDialog.show();
+			};
+
+			this.ankiCardsContainer.appendChild(clone);
+		}
 	},
 
 	show: function() {
+		this.dialog = document.importNode(this.dialogTemplate.content, true);
+		this.ankiCardsDialog = this.dialog.getElementById("ankiCardsDialog");
+		this.ankiCardsContainer = this.dialog.getElementById("ankiCardsContainer");
+
 		Overlay.setVisible(true);
-		Overlay.setPurpose(PURPOSE_SHOWCARDS);
-		fetch("cards" + textEditionInput.value)
+		Overlay.setContent(this.dialog);
+
+		fetch("cards")
 			.then(response => {return response.json()})
 			.then(json=> {
 				this.populate(json);
